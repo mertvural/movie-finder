96244a1fb989aca3f004ed1a340aa354d1e0d18e
mertvural <mertvural90@gmail.com>
2020-12-29 01:28:20 +0300

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                  use: ['style-loader','css-loader']
              },
              {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(jpg|jpeg|gif|png|ico)$/,
                 use: [
                   {
                     loader: 'file-loader',
